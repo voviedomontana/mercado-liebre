cf1f56c0d4d505383fc088d7567c3fa2ecc8ae12
PedroPuga01 <pedro070801@gmail.com>
2021-10-18 21:09:30 -0300

    repositorio mercadoLiebre

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const app = express ()
+const path = require ('path')
+const bodyParser = require('body-parser');
+
+const publicPath = path.resolve(__dirname,'./public');
+app.use(express.static(publicPath))
+
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname,'./views/home.html'));
+});
+
+app.get('/',function(req,res){
+    let dir = path.resolve(__dirname,"./views/home.html");
+    res.sendFile(dir)
+})
+
+app.get('/register',function(req,res){
+    let dir = path.join(__dirname,"./views/register.html");
+    res.sendFile(dir)
+})
+
+app.get('/login', function(req, res) {
+    let dir = path.join(__dirname,'./views/login.html');
+    res.sendFile(dir)
+});
+
+app.listen(5000,function(){
+    console.log("Servidor andando sin problemas en el puerto 5000")
+})
