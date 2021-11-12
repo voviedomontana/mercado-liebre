6fca762d6e659ef052685dbefd9bfbb800298c46
Valentino Oviedo <valenoviedo2001@gmail.com>
2021-11-11 21:54:41 -0300

    subiendo mercado-liebre

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path')
+const app = express();
+
+app.use(express.static('public'));
+
+app.listen(3050, ()=>{
+    console.log('Servidor funcionando en el puerto 3050');
+});
+
+app.get('/', (req,res)=>{
+    res.sendFile(__dirname + '/views/home.html');
+});
+app.get('/register', (req,res) => {
+    res.sendFile(path.join(__dirname,"/views/register.html"))
+});
